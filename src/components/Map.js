94c84590838bd9909373c4af496de6b29b2616c8
Yixin Zhang <yz2255@cornell.edu>
2021-10-23 20:54:18 -0400

    Add Map Layout

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,65 @@
 // to use JSX, import:
 import React from "react";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+// import "./Map.css";
 
-function simpleMap() {
+export default function simpleMap() {
   return (
-    <div>
-      <h2>This is the map</h2>
-    </div>
+    <Container>
+      <Row>
+        <Col><MapNavBar /></Col>
+      </Row>
+      <Row>
+        <Col><MapContainer /></Col>
+        <Col><SideList /></Col>
+      </Row>
+    </Container>
   );
 }
 
-export default simpleMap;
+function MapContainer() {
+  return (
+    <>
+      <h1>Map Container</h1>
+    </>
+  )
+}
+
+{/* export default simpleMap; */}
+
+function MapNavBar() {
+  return (
+    <Container>
+    <h1>MapNavBar Here</h1>
+    <Row>
+    <Col><SearchBar /></Col>
+    <Col><Filter /></Col>
+    </Row>
+    </Container>
+    )
+}
+
+function SideList () {
+  return <h1>SideList Here</h1>
+}
+
+function SearchBar() {
+  return <h1>SearchBar</h1>
+}
+
+function Filter() {
+  return (<>
+  <h1>Filter</h1>
+  <FilterComponent />
+  <FilterComponent />
+  <FilterComponent />
+  <FilterComponent />
+  </>
+  )
+}
+
+function FilterComponent () {
+  return <></>
+}
